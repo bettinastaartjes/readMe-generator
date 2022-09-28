a66b03ad5e8fec0c5b0511fe5a0e0f15b8dc342f
Bettina Staartjes <bbstaars@gmail.com>
2022-09-28 11:54:18 -0400

    added functions and questions but pushing to main

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,61 @@
-// TODO: Include packages needed for this application
+/* 1. TODO: Include packages needed for this application. 
+Go to root directory of folder and type <npm install inquierer@8.2.4> in the termainl to install inquirer*/
 
-// TODO: Create an array of questions for user input
-// what was here originally: const questions = [];
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
+const {generateMarkdown} = require("./Develop/utils/generateMarkdown.js");
 
+// 2. TODO: Create an array of questions for user input*/
+const questions = function(questionData){
+    return inquirer
+    .prompt (
+    [
+        {
+            type: 'input',
+            message: "What is the name of your project?",
+            name: "title"
+        },
+        {
+            type: 'input',
+            message: "Please provide a Table of Contents for this project.",
+            name: "tableOfContents"
+        },
+        {
+            type: 'input',
+            message: "What's your username?",
+            name: "username"
+        },
+        {
+            type: 'input',
+            message: "Please provide a description of the project.",
+            name: "projectDescripton"
+        },
+    ]);
+}
 
+/* 3. TODO: Create a function to write README file
+function writeToFile(fileName, data) {}*/
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    questions ()
+    .then(questionData => {
+        const pageMD = generateMarkdown(questionData)
+        fs.writeFile('./README.md', pageMD, err => {
+            if (err) throw new Error (err);
+            console.log('ReadMe File Created!');
+        })
+    })
+} 
 
-// TODO: Create a function to initialize app
-function init() {}
+/* TODO: Create a function to initialize app
+function init() {}*/
 
-// Function call to initialize app
-init();
+function init () {
+    writeToFile()
+}
 
+/* Function call to initialize app
+init()*/
+
+init ();
